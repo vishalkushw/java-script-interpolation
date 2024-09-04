3257086ef6d02815c2d3cef5df37945328e1c233
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-04 22:50:42 +0530

    Interpolation in javascript

---
+++ first.js
@@ -0,0 +1,7 @@
+let nam ="vishal kushwah";
+let age=25;
+let place ="wardha";
+let sub="B-tech compter science";
+
+let str=`my name is ${nam} i am from ${place} and my age is ${age} i am a student of ${sub}`;
+document.write(str);
